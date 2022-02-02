72a350b8584f3a288f9bfe33ca5586a5c61602f9
Gery Wahyu <gerywahyunugraha@gmail.com>
2022-02-02 19:14:24 +0800

    Refactor structure

--- components/pdfmerger/components/PDFMerger/PDFMerger.js
+++ components/pdfmerger/components/PDFMerger/PDFMerger.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { PDFDocument } from "pdf-lib";
 import download from "downloadjs"
+import { image } from "tailwindcss/lib/util/dataTypes";
 
 export default function PDFMerger() {
   const [pdf, setPdf] = useState(null)
@@ -8,48 +9,58 @@ export default function PDFMerger() {
   useEffect(() => {
     async function initPdf() {
       const pdfDoc = await PDFDocument.create()
-      const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg'
-      const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
-
-      // Fetch PNG image
-      const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png'
-      const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
-
-      // Embed the JPG image bytes and PNG image bytes
-      const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
-      const pngImage = await pdfDoc.embedPng(pngImageBytes)
-
-      // Get the width/height of the JPG image scaled down to 25% of its original size
-      const jpgDims = jpgImage.scale(0.25)
-
-      // Get the width/height of the PNG image scaled down to 50% of its original size
-      const pngDims = pngImage.scale(0.5)
-
-      // Add a blank page to the document
-      const page = pdfDoc.addPage()
-
-      // Draw the JPG image in the center of the page
-      page.drawImage(jpgImage, {
-        x: page.getWidth() / 2 - jpgDims.width / 2,
-        y: page.getHeight() / 2 - jpgDims.height / 2,
-        width: jpgDims.width,
-        height: jpgDims.height,
-      })
-
-      // Draw the PNG image near the lower right corner of the JPG image
-      page.drawImage(pngImage, {
-        x: page.getWidth() / 2 - pngDims.width / 2 + 75,
-        y: page.getHeight() / 2 - pngDims.height,
-        width: pngDims.width,
-        height: pngDims.height,
-      })
-
       setPdf(pdfDoc)
     }
     initPdf().then(r => {
       console.log('init pdf successfully')
     })
   }, [])
+
+  async function addImage(imageBytes, type) {
+    if (pdf == null) {
+      console.log('pdf is not init')
+      return
+    }
+
+    let image;
+    switch (type) {
+      case "jpg":
+        image = await getJpgImage(imageBytes)
+        break
+      case "png":
+        image = await getPngImage(imageBytes)
+        break
+      default:
+        return
+    }
+
+    const dims = image.scale(1)
+    const page = pdf.addPage()
+    page.drawImage(image, {
+      x: 0,
+      y: 0,
+      width: dims.width,
+      height: dims.height
+    })
+  }
+
+  async function getJpgImage(imageBytes) {
+    if (pdf == null) {
+      console.log('pdf not init')
+      return
+    }
+
+    return await pdf.embedJpg(imageBytes)
+  }
+
+  async function getPngImage(imageBytes) {
+    if (pdf == null) {
+      console.log('pdf not init')
+      return
+    }
+
+    return await pdf.embedPng(imageBytes)
+  }
 
   async function downloadPdf() {
     if (pdf == null) {
@@ -60,7 +71,25 @@ export default function PDFMerger() {
     download(pdfBytes, "pdf-lib_image_embedding_example.pdf", "application/pdf");
   }
 
+  async function addFirstMockImage() {
+    const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg'
+    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+
+    await addImage(jpgImageBytes, "jpg")
+  }
+
+  async function addSecondMockImage() {
+    const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png'
+    const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
+
+    await addImage(pngImageBytes, "png")
+  }
+
   return (
-    <button className="bg-black text-white rounded-md p-4" onClick={downloadPdf}>Download PDF</button>
+    <div className="flex flex-col space-y-2">
+      <button className="bg-black text-white rounded-md p-4" onClick={addFirstMockImage}>Add JPG Image</button>
+      <button className="bg-black text-white rounded-md p-4" onClick={addSecondMockImage}>Add PNG Image</button>
+      <button className="bg-black text-white rounded-md p-4" onClick={downloadPdf}>Download PDF</button>
+    </div>
   )
 }
