cb6db563aace76af013fd45321a23bb3d4969c7b
debbyalmadea <almadeaputri@gmail.com>
2023-01-05 22:53:31 +0800

    fix: ui

--- components/pdfmerger/components/PDFMerger/PDFMerger.js
+++ components/pdfmerger/components/PDFMerger/PDFMerger.js
@@ -1,153 +1,172 @@
 import { useEffect, useState } from "react";
 import { PDFDocument } from "pdf-lib";
-import download from "downloadjs"
+import download from "downloadjs";
 import { image } from "tailwindcss/lib/util/dataTypes";
 
 export default function PDFMerger() {
-  const [pdf, setPdf] = useState(null)
-  const [fileList, setFileList] = useState([])
+  const [pdf, setPdf] = useState(null);
+  const [fileList, setFileList] = useState([]);
 
   useEffect(() => {
     async function initPdf() {
-      const pdfDoc = await PDFDocument.create()
-      setPdf(pdfDoc)
+      const pdfDoc = await PDFDocument.create();
+      setPdf(pdfDoc);
     }
 
-    initPdf().then(r => {
-      console.log('init pdf successfully')
-    })
-  }, [])
+    initPdf().then((r) => {
+      console.log("init pdf successfully");
+    });
+  }, []);
 
   async function addImage(imageBytes, type) {
     if (pdf == null) {
-      console.log('pdf is not init')
-      return
+      console.log("pdf is not init");
+      return;
     }
 
     let image;
     switch (type) {
       case "jpg":
-        image = await getJpgImage(imageBytes)
-        break
+        image = await getJpgImage(imageBytes);
+        break;
       case "png":
-        image = await getPngImage(imageBytes)
-        break
+        image = await getPngImage(imageBytes);
+        break;
       default:
-        return
+        return;
     }
 
-    const dims = image.scale(1)
-    const page = pdf.addPage()
+    const dims = image.scale(1);
+    const page = pdf.addPage();
     page.drawImage(image, {
       x: 0,
       y: 0,
       width: dims.width,
-      height: dims.height
-    })
+      height: dims.height,
+    });
   }
 
   async function getJpgImage(imageBytes) {
     if (pdf == null) {
-      console.log('pdf not init')
-      return
+      console.log("pdf not init");
+      return;
     }
 
-    return await pdf.embedJpg(imageBytes)
+    return await pdf.embedJpg(imageBytes);
   }
 
   async function getPngImage(imageBytes) {
     if (pdf == null) {
-      console.log('pdf not init')
-      return
+      console.log("pdf not init");
+      return;
     }
 
-    return await pdf.embedPng(imageBytes)
+    return await pdf.embedPng(imageBytes);
   }
 
   async function downloadPdf() {
     if (pdf == null) {
-      console.log('pdf is not initiated')
-      return
+      console.log("pdf is not initiated");
+      return;
     }
-    const pdfBytes = await pdf.save()
-    download(pdfBytes, "pdf-lib_image_embedding_example.pdf", "application/pdf");
+    const pdfBytes = await pdf.save();
+    download(
+      pdfBytes,
+      "pdf-lib_image_embedding_example.pdf",
+      "application/pdf"
+    );
   }
 
   async function addFirstMockImage() {
-    const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg'
-    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+    const jpgUrl = "https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg";
+    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer());
 
-    await addImage(jpgImageBytes, "jpg")
+    await addImage(jpgImageBytes, "jpg");
   }
 
   async function addSecondMockImage() {
-    const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png'
-    const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
+    const pngUrl = "https://pdf-lib.js.org/assets/minions_banana_alpha.png";
+    const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer());
 
-    await addImage(pngImageBytes, "png")
+    await addImage(pngImageBytes, "png");
   }
 
   async function onFileUpload(input) {
-    setFileList(input)
-    console.log(input)
+    setFileList(input);
+    console.log(input);
 
-    Array.from(input).forEach(file => {
-      const reader = new FileReader()
-      const fileType = file.type.replace("image/", "")
+    Array.from(input).forEach((file) => {
+      const reader = new FileReader();
+      const fileType = file.type.replace("image/", "");
 
       reader.onerror = () => {
-        console.error("failed to read file to buffer", file, reader.error)
-      }
+        console.error("failed to read file to buffer", file, reader.error);
+      };
 
       reader.onload = () => {
         if (reader.result == null) {
-          console.error("file result is null", file, reader.error)
-          return
+          console.error("file result is null", file, reader.error);
+          return;
         }
 
-        console.info("successfully read file", file)
+        console.info("successfully read file", file);
         if (fileType === "jpeg") {
-          addImage(reader.result, "jpg")
+          addImage(reader.result, "jpg");
         } else if (fileType === "png") {
-          addImage(reader.result, "png")
+          addImage(reader.result, "png");
         }
 
-        console.info("successfully embed file to pdf", file)
-      }
+        console.info("successfully embed file to pdf", file);
+      };
 
-      reader.readAsArrayBuffer(file)
-    })
+      reader.readAsArrayBuffer(file);
+    });
   }
 
   return (
-    <div className="container">
-      <h1 className="title">Merge PDF</h1>
-      <h2 className="sub-title">Convert JPG/PNG images to PDF in seconds</h2>
-      <div className="buttons">
-
-        <form className="form">
-          <label className="input-label" htmlFor="upload">Upload JPG/PNG Files</label>
-          <input id="upload" type="file" className="input-file"
-                 multiple
-                 onChange={(e) => onFileUpload(e.target.files)}
+    <div>
+      <h1 className="lg:text-8xl text-4xl font-extrabold text-center">
+        <span className="underline">Quick</span> PDF
+      </h1>
+      <p className="lg:text-2xl text-lg text-center my-4">
+        Convert JPG/PNG images to PDF in seconds
+      </p>
+      <div className="flex flex-col justify-center items-center">
+        <button className="relative w-full bg-red-500 py-4 rounded-2xl">
+          <label
+            className="text-center lg:text-2xl text-lg text-white w-full"
+            htmlFor="upload"
+          >
+            Upload JPG/PNG Files
+          </label>
+          <input
+            id="upload"
+            type="file"
+            className="absolute z-[-1] top-0 left-0"
+            multiple
+            onChange={(e) => onFileUpload(e.target.files)}
           />
-        </form>
+        </button>
 
-        <ul className="uploaded-file-list">
-          {fileList.length > 0 && (
+        <ul className="w-full my-2 font-light lg:text-md text-sm">
+          {fileList.length > 0 &&
             Object.keys(fileList).map((key) => (
-              <li className="uploaded-file" key={key}>
+              <li className="text-start" key={key}>
                 {fileList[key].name}
               </li>
-            ))
-          )}
+            ))}
           {fileList.length === 0 && (
-            <li className="no-result">No file uploaded. yet.</li>
+            <li className="text-start">No file uploaded. yet.</li>
           )}
         </ul>
 
-        <button className="download" onClick={downloadPdf}>Download PDF</button>
+        <button
+          className="w-full bg-red-500 py-4 rounded-2xl lg:text-2xl text-lg text-white"
+          onClick={downloadPdf}
+        >
+          Download PDF
+        </button>
       </div>
     </div>
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import '../styles/PDFMerger.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
