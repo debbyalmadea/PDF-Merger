f90d21f4c05cdbefa13e0c8417c5c84adab0301a
debbyalmadea <almadeaputri@gmail.com>
2023-01-17 00:27:03 +0700

    feat: integrate typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import PDFMerger from "../components/pdfmerger/components/PDFMerger/PDFMerger";
+import { NextPage } from "next";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className="w-screen h-screen flex flex-row justify-center items-center lg:px-16 px-8">
       <PDFMerger />
     </div>
   );
-}
+};
+
+export default Home;
