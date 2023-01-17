ffccb1760246e352415273c66dcba0840f5a2d42
debbyalmadea <almadeaputri@gmail.com>
2023-01-17 21:12:38 +0700

    feat: integrate pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+});
